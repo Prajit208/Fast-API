929c3b80099fcfa3820a9f7e2ea24e8d6315433e
Prajit <kcprajit208@gmail.com>
2026-05-27 12:19:23 +0545

    student CRUD API complete

--- student.py
+++ student.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI,Query
-
+from pydantic import BaseModel
 from typing import Annotated
 app=FastAPI()
 
+
+class Students(BaseModel):
+    
+    name: str
+    age: int
+    grade: str
+
 students = {
-    1: "Prajit",
-    2: "Ram",
-    3: "Hari"
+    1: {"name": "Prajit", "age": 21, "grade": "A"},
+    2: {"name": "Ram", "age": 20, "grade": "B"},
+    3: {"name": "Hari", "age": 22, "grade": "A"}
 }
+
 @app.get("/")
 async def root():
     return{"message":"API is running"}
@@ -18,12 +26,7 @@ async def students_display(name: str| None=None):
         return{"Student": [s for s in students.values() if s==name]}
     return {"students": students}
 
-# @app.get("/students/{student_id}")
-# async def display_id(student_id : int,q: Annotated[str | None, Query(max_length=50)]= None ):
-#     result={"Student ID":student_id}
-#     if q:
-#         result.update({"q": q})
-#     return result    
+
 
 @app.get("/students/{student_id}")
 async def display_id(student_id: int):
@@ -31,4 +34,32 @@ async def display_id(student_id: int):
         return {"student": students[student_id]}
     return {"error": "Student not found"}
 
-    
+@app.post("/students")
+async def add_student(student: Students):
+    new_id=max(students.keys())+1
+    # update the existing dictionary
+    students[new_id]={"name":student.name,
+                        "age":student.age,
+                        "grade":student.grade}
+    return{
+            "id":new_id,**students[new_id] # ** unpacks a python dictionary
+        
+    }
+
+@app.put("/students/{student_id}") 
+async def update_student(student_id: int,student: Students):
+    if student_id in students:  
+        students[student_id]={"name":student.name,
+                        "age":student.age,
+                        "grade":student.grade} 
+        return{
+            "id":student_id,**students[student_id]
+        }
+    return{"error":"Student not found"}        
+
+@app.delete("/students/{student_id}")
+async def delete_student(student_id: int):
+    if student_id in students:
+        del students[student_id]
+        return{"message":"Deleted"}
+    return{"error":"Student not found"} 
